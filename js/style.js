d0a634b5be9899eea4a821edff19dc185c4726e4
lxf <617105706@qq.com>
2018-04-28 00:06:57 +0800

    two

--- js/style.js
+++ js/style.js
@@ -16,43 +16,51 @@ function refreshRem() {
 }
 
 $(function() {
-    //分屏
-    var swiper = new Swiper('.swiper-container-v', {
+    // 横版-卡牌
+    var swiperH = new Swiper('.swiper-container-h', {
+        prevButton: '.swiper-button-prev',
+        nextButton: '.swiper-button-next',
+        spaceBetween: 300,
+        onInit: function(swiperH) { //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiperH); //隐藏动画元素
+            swiperAnimate(swiperH); //初始化完成开始动画
+        },
+        onSlideChangeEnd: function(swiperH) {
+            swiperAnimate(swiperH); //每个slide切换结束时也运行当前slide动画
+        }
+    });
+    //分屏-竖版
+    var swiperV = new Swiper('.swiper-container-v', {
         pagination: null,
         direction: 'vertical',
-        onInit: function(swiper) {
-            swiperAnimateCache(swiper); //隐藏动画元素
-            swiperAnimate(swiper); //初始化完成开始动画
+        onInit: function(swiperV) {
+            // swiperAnimateCache(swiperV); //隐藏动画元素
+            // swiperAnimate(swiperV); //初始化完成开始动画
+            // alert(swiperV.activeIndex); //提示Swiper的当前索引
         },
-        onSlideChangeEnd: function(swiper) {
-            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-        },
-        onSlideChangeEnd: function(swiper) {
-            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
-            // alert(swiper.activeIndex) //切换结束时，告诉我现在是第几个slide
-            $('.tab_list li').eq(swiper.activeIndex).addClass('active').siblings().removeClass('active');
+        onSlideChangeEnd: function(swiperV) {
+            // swiperAnimate(swiperV); //每个slide切换结束时也运行当前slide动画
+            // alert(swiperV.activeIndex) //切换结束时，告诉我现在是第几个slide
+            $('.tab_list li').eq(swiperV.activeIndex).addClass('active').siblings().removeClass('active');
+
+            if (swiperV.activeIndex == 2) {
+                //alert('aa')
+            }
         }
     });
 
-    var swiperH = new Swiper('.swiper-container-h', {
-        // pagination: '.swiper-pagination-h',
-        // paginationClickable: true,
-        prevButton: '.swiper-button-prev',
-        nextButton: '.swiper-button-next',
-        spaceBetween: 300
-    });
+
 
     function swiper_tab(ele) {
         $(ele).on('click', function() {
             $(this).addClass('active').siblings().removeClass('active');
             let index = $(this).index();
             console.log(index)
-            swiper.slideTo(index, 1000, false); //切换到第一个slide，速度为1秒
+            swiperV.slideTo(index, 1000, false); //切换到第一个slide，速度为1秒
         })
     }
 
     swiper_tab('.tab_list li')
-
 
 
 
